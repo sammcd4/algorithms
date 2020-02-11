81a2ee43653608e7536a1218840b805c728fa707
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2020-02-10 21:32:43 -0500

    first file

---
+++ first_nonrepeating_character.py
@@ -0,0 +1,21 @@
+# First non-repeating character
+
+def first_nonrepeating_character(a_str):
+    char_map = {}
+    for c in a_str:
+        if char_map.get(c) is None:
+            char_map[c] = 1
+        else:
+            char_map[c] += 1
+
+    for c in a_str:
+        if char_map[c] == 1:
+            return c
+
+# Run my algorithm
+my_string = 'somethingthatiswrittenorsaid'
+answer = first_nonrepeating_character(my_string)
+
+# Print my result
+print('Original string is: {}'.format(my_string))
+print('The first non-repeating character is: {}'.format(answer))
