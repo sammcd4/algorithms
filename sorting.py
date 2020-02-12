cb022e1814cf2cfcba0601af19ca85cbdb1ed9c6
Sam McDonald <samuel.jacob.mcdonald@gmail.com>
2020-02-11 19:48:21 -0500

    Add sorting algorithms script

---
+++ sorting.py
@@ -0,0 +1,15 @@
+# Collection of sorting algorithms
+
+def bubblesort(object):
+    pass
+
+def quicksort(object):
+    pass
+
+def builtinsort(object):
+    return sorted(object)
+
+list = [4, 1, 3, 8, 0]
+ordered_list = bubblesort(list)
+
+print(ordered_list)
